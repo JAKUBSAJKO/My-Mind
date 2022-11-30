762850b6564f3a35eedbb1bed7741705636e1a62
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-11-30 11:11:52 +0100

    User have to be logged if he wanted to go to homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,17 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import CreatePost from "../components/CreatePost";
 import { UsersContext } from "../contexts/context";
+import { routes } from "../routes/routes";
 
 const Homepage: FC = () => {
+  const navigate = useNavigate();
   const usersContext = useContext(UsersContext);
+
+  useEffect(() => {
+    if (usersContext?.activeUser === null) navigate(routes.login);
+  }, []);
+
   return (
     <div className="bg-day-bg-posts">
       <div className="bg-day-bg-main-posts shadow-day-shadow-posts mx-64 p-16 text-sm flex flex-col justify-center items-center gap-4">
