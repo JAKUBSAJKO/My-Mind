752c2a78d8c0850e976a6cc5a1247fb9b158c86c
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-12-08 22:31:20 +0100

    [TASK-021]: RWD for modal.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,7 +9,7 @@ const Modal: FC<Props> = ({ children }) => {
   return createPortal(
     <>
       <div className="fixed top-0 bottom-0 left-0 right-0 bg-modal-overlay" />
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white text-black p-4 rounded-lg">
+      <div className="fixed w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white text-black p-4 rounded-lg scale-90 sm:w-auto sm:scale-100">
         {children}
       </div>
     </>,
