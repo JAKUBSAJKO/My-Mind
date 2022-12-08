ef580575b738ab31a01cc91c51ccb05d7ccbaed1
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-12-08 21:28:11 +0100

    [TASK-019]: RWD for login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,7 +7,7 @@ import loginCharacter from "../assets/images/loginCharacter.png";
 
 const Login: FC = () => {
   return (
-    <div className="w-full h-[calc(100vh-82.5px)] flex justify-center items-center px-36">
+    <div className="w-full flex flex-col justify-center items-center px-4 py-16 gap-12 sm:gap-16 lg:h-[calc(100vh-82.5px)] lg:flex-row lg:px-36 lg:py-0 xl:max-w-5xl xl:mx-auto">
       <div className="basis-1/2 flex flex-col">
         <h1 className="text-4xl font-semibold mb-2">Sign in to</h1>
         <h2 className="text-3xl font-medium">My mind is simple</h2>
@@ -15,7 +15,7 @@ const Login: FC = () => {
           src={loginCharacter}
           alt="Login Character"
           width={220}
-          className="text-end"
+          className="self-center md:self-start"
         />
         <p className="text-sm">If you don't have an account register</p>
         <p className="text-sm">
