c9299d1de5abacacfc32eb553b47857e77829f87
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-12-15 16:13:59 +0100

    [TASK-036]: Create initial data for posts and users.

---
+++ src/data/posts.js
@@ -0,0 +1,124 @@
+import { initialUsers as users } from "./users.js";
+
+export const initialPosts = [
+  {
+    id: "0000-0001",
+    user: users[0],
+    body: "Ludzkie oczy nie zmieniają swojego rozmiaru od urodzenia.",
+    date: "28-11-2022",
+  },
+  {
+    id: "0000-0002",
+    user: users[1],
+    body: "Kichnięcie 'podróżuje' z prędkością około 44 m/s. Wow to jest niesamowite",
+    date: "29-11-2022",
+  },
+  {
+    id: "0000-0003",
+    user: users[2],
+    body: "Nie wiem czy ktoś słyszał o tym, ale podobno w singapurze nie dozwolone jest żucie gumy.",
+    date: "30-11-2022",
+  },
+  {
+    id: "0000-0004",
+    user: users[3],
+    body: "Szminki do ust mogą zawierać rybie łuski.",
+    date: "01-12-2022",
+  },
+  {
+    id: "0000-0005",
+    user: users[4],
+    body: "W trakcie oddychania żebra poruszają się rocznie pięć milionów razy. Wow to na prawdę dużo",
+    date: "02-12-2022",
+  },
+  {
+    id: "0000-0006",
+    user: users[3],
+    body: "Najwięcej rozmów telefonicznych rejestrowanych jest zawsze w Dzień Matki.",
+    date: "03-12-2022",
+  },
+  {
+    id: "0000-0007",
+    user: users[5],
+    body: "Drugi września to międzynarodowy dzień lodów o smaku borówki.",
+    date: "04-12-2022",
+  },
+  {
+    id: "0000-0008",
+    user: users[6],
+    body: "Przeciętny czterolatek zadaje ponad 400 pytań dziennie. ",
+    date: "05-12-2022",
+  },
+  {
+    id: "0000-0009",
+    user: users[7],
+    body: "Woda kokosowa może być stosowana jako osocze krwi. ",
+    date: "06-12-2022",
+  },
+  {
+    id: "0000-0010",
+    user: users[8],
+    body: "Gdzieś ostatnio widziałam ciekawostę, że kobiety mają więcej kubków smakowych niż mężczyźni. To bardzo ciekawe.",
+    date: "07-12-2022",
+  },
+  {
+    id: "0000-0011",
+    user: users[9],
+    body: "Pięć! Dokładnie tylko planet jesteśmy w stanie zobaczyć gołym okiem. A są nimi: Wenus, Merkury, Mars, Jowisz, Saturn.",
+    date: "08-12-2022",
+  },
+  {
+    id: "0000-0012",
+    user: users[4],
+    body: "Ziemia jest jedyną plantą w naszym układzie słonecznym, której nazwa nie pochodzi od imienia boga.",
+    date: "09-12-2022",
+  },
+  {
+    id: "0000-0013",
+    user: users[2],
+    body: "W chinach zakazane jest przytulanie drzew.",
+    date: "10-12-2022",
+  },
+  {
+    id: "0000-0014",
+    user: users[7],
+    body: "Ludzie, którzy pracują więcej niż 11 godzin dziennie, mają o 67 proc. większą szansę na atak serca niż ci, którzy pracują tylko osiem godzin. ",
+    date: "11-12-2022",
+  },
+  {
+    id: "0000-0015",
+    user: users[1],
+    body: "Ostatnio byłem we Francji. Miałem okazję zwiedzić wieżę Eiffla. Nie wiem czy wiecie, ale aby wejść na tę wieżę trzeba pokonoać 1710 stopnia. To naprawdę dużo schodów :D",
+    date: "12-12-2022",
+  },
+  {
+    id: "0000-0016",
+    user: users[6],
+    body: "Nawiązując do ciekawostki jedengo z was w 1967 roku wieża Eiffla została czasowo przeniesiona do Kanady.",
+    date: "13-12-2022",
+  },
+  {
+    id: "0000-0017",
+    user: users[9],
+    body: "Widelec pojawił się we Włoszech wcześniej niż w jakimkolwiek innym kraju. Wiadomo, że to ze względu na marakon ;p",
+    date: "14-12-2022",
+  },
+  {
+    id: "0000-0018",
+    user: users[2],
+    body: "Leonardo da Vinci potrafił malować jedną ręką i jednocześnie pisać drugą. Niesamowity gość ;p",
+    date: "14-12-2022",
+  },
+  {
+    id: "0000-0019",
+    user: users[7],
+    body: "Od 1945 roku wszystkie brytyjskie czołgi są wyposażone w zestaw do parzenia herbaty. Widamo zawsze jest pora na herbatkę ;p",
+    date: "15-12-2022",
+  },
+  {
+    id: "0000-0020",
+    user: users[4],
+    body: "Ostatnio oglądałam Pulp Fiction Quentina Tarantino, którego jestem wielką fanką. W filmie tym wszystkie zegarki są ustawione na 4:20. Ciekawa jestem ile osób oglądających ten film to zauważyło :D",
+    date: "15-12-2022",
+  },
+];
